38dc46c2ab4162a1c0fc378f21d0e5b8da034a3e
cice <sermarmar>
2018-07-10 21:30:06 +0200

    Modificacion

--- src/main/java/es/gmdesign/spring/zuulserver/ZuulServerApplication.java
+++ src/main/java/es/gmdesign/spring/zuulserver/ZuulServerApplication.java
@@ -3,11 +3,12 @@ package es.gmdesign.spring.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
